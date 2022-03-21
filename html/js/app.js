8f9ff0002ea274a6ef75d63e0fe7de347b015160
SYNO <89760730+SYNOGIM@users.noreply.github.com>
2022-03-21 12:01:23 +0400

    Add files via upload

---
+++ html/js/app.js
@@ -0,0 +1,16 @@
+let innerCursor = document.querySelector(".inner-cursor");
+let outerCursor = document.querySelector(".outer-cursor");
+
+document.addEventListener("mousemove", moveCursor);
+
+function moveCursor(e){
+    let x = e.clientX;
+    let y = e.clientY;
+    console.log(x,y)
+    innerCursor.style.left = `${x}px`;
+    innerCursor.style.top = `${y}px`;
+    outerCursor.style.left = `${x}px`;
+    outerCursor.style.top = `${y}px`;
+}
+
+
